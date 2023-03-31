f8483d5aa7c878937664570c39a7ac0c6bea8a93
Priyankamahajan11 <82284404+Priyankamahajan11@users.noreply.github.com>
2023-03-31 11:20:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import bodyParser from "body-parser";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 //components
-import Connection from "./database/db.js";
-import Router from "./routes/route.js";
+import Connection from './database/db.js';
+import Router from './routes/route.js';
+
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", Router);
+app.use('/', Router);
 
-const PORT = process.env.PORT || 8000;
+
+const PORT = 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
-const URL =
-  process.env.MONGO_URI ||
-  `mongodb+srv://${username}:${password}@cluster0.on5bux7.mongodb.net/?retryWrites=true&w=majority`;
 
-app.listen(PORT, () =>
-  console.log(`Server is running successfully on PORT ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
